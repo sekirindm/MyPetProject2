8c3855e35ea2c8d80b3c9780978988591b677450
dmsekir <sekirindimka@mail.ru>
2023-05-24 18:42:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mypetproject2/features/SplashScreenFragment.kt
@@ -0,0 +1,60 @@
+package com.example.mypetproject2.features
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mypetproject2.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SplashScreenFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class SplashScreenFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            requireActivity().supportFragmentManager.beginTransaction().remove(this).commit() // Закрываем заставку
+        }, SPLASH_SCREEN_DELAY)
+    }
+
+    companion object {
+         val SPLASH_SCREEN_DELAY: Long = 2000
+
+
+        fun newInstance(param1: String, param2: String) =
+            SplashScreenFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
